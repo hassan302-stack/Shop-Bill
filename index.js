4a9c365ccd67e36fbbb29d18993ebb5023260208
hassan302-stack <kazmihassan336@gmail.com>
2021-07-13 01:33:00 +0500

    timer

--- index.js
+++ index.js
@@ -7,8 +7,19 @@
   var para =document.getElementById('para')
  var txt =document.getElementById('txt')
  var select = document.getElementById('select')
-
+ var num=0
  btn.addEventListener('click',()=>{
-     para.innerHTML = `Mr. ${txt.value} select ${select.value} and quantity is ${quant.value}
-     your bill is ${quant.value*5} Pkr,Thanks`
- })
+    
+    para.innerHTML ="Loading ....."
+     var time = setInterval(()=>{
+        if(num===5){
+            clearInterval(time)
+            alert('time out')}
+         para.innerHTML =` quiz finish after 5s now time is ${num++}s`
+         
+     },1000)
+    
+    }
+   
+ )
+ 
